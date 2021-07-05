df70ab332f648b9b918aeee4262dbd0ea41c5726
Pororo-droid <yjwsis@kookmin.ac.kr>
2021-07-06 01:41:17 +0900

    [SETTING] Spring boot

---
+++ ChatServer/src/test/java/com/Pororo/ChatServer/ChatServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Pororo.ChatServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
